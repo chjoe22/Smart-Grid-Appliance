44e5be6b61081e07071a71e7abbf37ca32c1d37b
chjoe22 <chjoe22@student.sdu.dk>
2025-03-10 15:08:36 +0100

    the initials
    The project now includes vite and spring boot

---
+++ backend/src/main/java/sdu/mmmi/sga/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package sdu.mmmi.sga.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/main/java/sdu/mmmi/sga/backend/BackendController.java
@@ -0,0 +1,13 @@
+package sdu.mmmi.sga.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BackendController {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
+}

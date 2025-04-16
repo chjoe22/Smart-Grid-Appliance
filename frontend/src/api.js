baa859c4351ee677ebc28b229eae8020d5a633ed
chjoe22 <chjoe22@student.sdu.dk>
2025-04-16 21:04:04 +0200

    testing main to db

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: '/api/v1',
+    baseURL: '/api',
 });
 
 export const getExampleData = async () => {
     try {
         // Fetch data from the API and change the endpoint to match your API
-        const response = await api.get('/users');
+        const response = await api.get('/data');
         return response.data;
     } catch (error) {
         console.error('Error fetching data: ', error);

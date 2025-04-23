49b43b1cdc81fc2545a9cfb0dbcae9aa0429a2f0
Mads Wrang Sigsgaard <mads54325@gmail.com>
2025-04-23 16:41:25 +0200

    Basic react routing with modification to Loginpage so its centered my an Absolute value (This is not the correct way to do it. But it works for now). Also made the paths that were absolute to relative

--- frontend/src/components/MainPage/MainPage.jsx
+++ frontend/src/components/MainPage/MainPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MainPage = () => {
     return (
-        <div className="w-4/5 h-screen bg-gray-100 p-6">
+        <div className="w-4/5 h-screen bg-blue-100 p-6">
             <h2 className="text-center text-xl font-semibold italic">Main Page</h2>
             <div className="mt-4 grid grid-cols-2 gap-4">
                 <div className="bg-green-300 p-4 rounded-lg h-64">Graph 1</div>
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,4 @@
+// src/pages/HomePage.jsx
+export default function HomePage() {
+    return <h1>Welcome to the Home Page!</h1>;
+}
